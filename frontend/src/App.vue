8381465c3f73e90966727f96b944ae9166c72794
HiagoCS <hiago8cs@gmail.com>
2023-01-14 23:38:03 -0300

    vue-router configurado

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,7 +3,10 @@
 </script>
 
 <template>
-
+  <div id="app">
+    <h1>App Default</h1>
+    <router-view></router-view>
+  </div>
 </template>
 
 <style scoped>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import {createRouter, createWebHistory} from 'vue-router'
 import './assets/main.css'
 
-createApp(App).mount('#app')
+//Pages
+
+
+const routes = [
+    
+]
+const router = createRouter({
+    history:createWebHistory(),
+    routes
+})
+
+createApp(App).use(router).mount('#app')
+
