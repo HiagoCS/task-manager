6a26d31813ceb856b25146a82b79478c88f6fc6a
HiagoCS <hiago8cs@gmail.com>
2023-01-15 23:04:21 -0300

    pagina de login

---
+++ frontend/src/components/pages/login/LoginComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="main">
+        <br>
+        <h1 class="titulo">Login</h1>
+
+        <div class="form">
+            <div class="input-box">
+            <span class="label">Nome</span>
+            <input type="text">
+            </div>
+            <div class="input-box">
+                <span class="label">Senha</span>
+                <input type="text">
+            </div>
+            <button>Entrar</button>
+            <a href="/register">Ainda não tem conta? Clique aqui</a>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" src="./style.scss" scoped></style>
