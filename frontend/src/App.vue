7ae87938a740b0539d778cf783a88bfa943b39d5
HiagoCS <hiago8cs@gmail.com>
2023-01-15 23:05:01 -0300

    configurado router-view

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,6 @@
-<script setup>
-
-</script>
-
 <template>
   <div id="app">
-    <h1>App Default</h1>
     <router-view></router-view>
   </div>
 </template>
-
-<style scoped>
-
-</style>
+<style lang="scss" src="./assets/scss/_main.scss"></style>
