bda0e06090e5dce6199f173b4a82ce36332c33d4
HiagoCS <hiago8cs@gmail.com>
2023-01-15 23:15:48 -0300

    pagina de cadastro

---
+++ frontend/src/components/pages/register/RegisterComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="main">
+        <br>
+        <h1 class="titulo">Cadastre-se</h1>
+
+        <div class="form">
+            <div class="input-box">
+            <span class="label">E-mail</span>
+            <input type="text">
+            </div>
+            <div class="input-box">
+            <span class="label">Nome</span>
+            <input type="text">
+            </div>
+            <div class="input-box">
+                <span class="label">Senha</span>
+                <input type="text">
+            </div>
+            <button>Entrar</button>
+            <a href="/login">Já tem conta? Clique aqui</a>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" src="./style.scss" scoped></style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,9 +4,11 @@ import {createRouter, createWebHistory} from 'vue-router'
 
 //Pages
 import LoginComponent from './components/pages/login/LoginComponent.vue';
+import RegisterComponent from './components/pages/register/RegisterComponent.vue';
 
 const routes = [
-    {path:'/login', component:LoginComponent}
+    {path:'/login', component:LoginComponent},
+    {path:'/register', component:RegisterComponent}
 ]
 const router = createRouter({
     history:createWebHistory(),
