8bfee9b4430b3cc94f9ae355b3380c75f97bbd54
HiagoCS <hiago8cs@gmail.com>
2023-01-21 01:19:41 -0300

    adicionado VueSweetAlert e criado componente addUser

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,15 +2,19 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import {createRouter, createWebHistory} from 'vue-router'
 
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 //Pages
 import Home from './components/pages/home/HomeComponent.vue';
 import CreateUserComponent from './components/pages/user/createUser/CreateUserComponent.vue';
 import ListUser from './components/pages/home/main-pages/listUsers/ListUserComponent.vue';
 import ListTask from './components/pages/home/main-pages/listTasks/ListTasksComponent.vue';
+import AddUser from './components/pages/home/main-pages/addUser/AddUserComponent.vue';
 
 
 const routes = [
-    {path:'/home/:option', component:Home},
+    {path:'/', component:Home},
+    {path:'/:option', component:Home},
     {path:'/new-user', component:CreateUserComponent}
 ]
 const router = createRouter({
@@ -18,5 +22,4 @@ const router = createRouter({
     routes
 })
 
-createApp(App).component('ListUser', ListUser).component('ListTask', ListTask).use(router).mount('#app')
-
+createApp(App).component('ListUser', ListUser).component('ListTask', ListTask).component('addUser', AddUser).use(router).use(VueSweetalert2).mount('#app');
