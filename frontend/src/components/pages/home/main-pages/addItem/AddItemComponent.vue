ecbaeb44b680691eb7808944c4234ba78e51aebd
HiagoCS <hiago8cs@gmail.com>
2023-01-21 06:34:47 -0300

    botão de adição universal adicionado

---
+++ frontend/src/components/pages/home/main-pages/addItem/AddItemComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="main-component" @click="this.$router.push(`${name}`)">
+        <div class="user-box">
+            <text>+</text>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" src="./style.scss" scoped></style>
+
+<script>
+    export default{
+        props:['name']
+    }
+</script>
--- frontend/src/components/pages/home/main-pages/addUser/AddUserComponent.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="main-component" @click="this.$router.push('/new-user')">
-        <div class="user-box">
-            <text>+</text>
-        </div>
-    </div>
-</template>
-
-<style lang="scss" src="./style.scss" scoped></style>
